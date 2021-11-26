ae8ab2ec052c3ba7a3c7b468d38ecbabee48b2c1
Tin Gun <ztingun@yahoo.com>
2021-11-26 12:40:21 -0500

    manual input data

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 # Ejercicio 2:  
 
-# Basado en una matriz de R x C, entregando coordenadas de X, Y,
-# se debe sumar los valores hacia las coordenadas de origen (0,0). 
-
-# Dominio ℕ = {0, 1, 2, 3, 4, …} 
-# El llenado de la matriz esta dado por Z: 
-# Cuando R = 1 , completar fila con Z,  si R > 1 entonces completar fila con: Z= Z + R –1
-
 import os
 
 def run(R,C,Z,x,y): # This function calculates the final result
@@ -37,13 +30,40 @@ def validation(R,C,x,y): # This function validate that the input parameters are 
 
 if __name__=="__main__":
     os.system("cls")
+    print(
+        """ Basado en una matriz de R x C, entregando coordenadas de X, Y,
+        se debe sumar los valores hacia las coordenadas de origen (0,0). 
+
+        Dominio N = {0, 1, 2, 3, 4, …} 
+        El llenado de la matriz esta dado por Z: 
+        Cuando R = 1 , completar fila con Z,  si R > 1 
+        entonces completar fila con: Z= Z + R –1
+
+        Ejemplo:  
+
+        Matriz R x C = 4, 3   
+        Z = 2    X = 1     Y = 2 
+
+        Por tanto, se obtiene la siguiente matriz:  
+                C=1    C=2    C=3 
+        R = 1    2      2      2 
+        R = 2    3      3      3
+        R = 3    4      4      4
+        R = 4    5      5      5 
+
+        Dada las coordenadas X e Y la matriz da como resultado 18 : 
+               x0      x1 
+        y0     2       2  
+        y1     3       3
+        Y2     4       4
+        """)
     #-------------------------------------parameters
-    R:int=4
-    C:int=3
-    Z:int=2
+    R=int(input('Ingrese el valor de R: '))
+    C=int(input('Ingrese el valor de C: '))
+    Z=int(input('Ingrese el valor de Z: '))
     #-------------------------------------coordinates
-    x:int=1
-    y:int=2
+    x=int(input('Ingrese el valor de x: '))
+    y=int(input('Ingrese el valor de y: '))
 
     if validation(R,C,x,y):
         res=run(R,C,Z,x,y)
